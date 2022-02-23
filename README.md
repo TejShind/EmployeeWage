60d09302dcc94d9bbebcb1314fa3be44bb2c0b72
TejShind <tej.gholap25@gmail.com>
2022-02-24 00:45:22 +0530

    [Tejaswini]ADDED:Feature for Daily employee Wage

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # EmployeeWage
 1) Welcome to the Program.
 2) Check Employee attendance.
+3) Add Daily employee wage.
--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -1,15 +1,22 @@
 package com.blz.employeewage;
 
 public class EmployeeWage {
+    public static final int FULL_TIME = 1;
+    public static final int EMP_WAGE_PER_HR = 20;
+
     public static void main(String[] args) {
+        int empHr = 0;
+        int empWage = 0;
         System.out.println("Welcome to Employee Wage Computation Program");
-        int Present = 1;
         int Empcheck = (int) (Math.random() * 10) % 2;
 
-        if (Empcheck == Present) {
+        if (Empcheck == FULL_TIME) {
+            empHr = 8;
             System.out.println("Employee is present");
         } else {
             System.out.println("Employee is Absent");
         }
+        empWage = empHr * EMP_WAGE_PER_HR;
+        System.out.println("Employee's wage is " + empWage);
     }
 }
