3471a5b68b091dffddbfe9eb3f618a65bfcec188
TejShind <tej.gholap25@gmail.com>
2022-02-24 00:29:26 +0530

    [Tejaswini]ADDED:Feature for Check employee attendance

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -3,5 +3,13 @@ package com.blz.employeewage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int Present = 1;
+        int Empcheck = (int) (Math.random() * 10) % 2;
+
+        if (Empcheck == Present) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
