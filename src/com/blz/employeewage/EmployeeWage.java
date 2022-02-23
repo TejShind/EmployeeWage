af9e3b155d2f7266a31f5bb5707384cffd112fcf
TejShind <tej.gholap25@gmail.com>
2022-02-24 01:32:25 +0530

    [Tejaswini]ADDED:Feature for Calculate wages till condition met

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -5,26 +5,35 @@ public class EmployeeWage {
     public static final int PART_TIME = 2;
     public static final int EMP_WAGE_PER_HR = 20;
     public static final int MAX_WORKING_DAYS = 20;
+    public static final int MAX_WORKING_HRS = 100;
 
     public static void main(String[] args) {
         int empHr = 0;
         int empWage = 0;
-        int totalWorkingDays = 1, totalEmpWage = 0;
+        int totalWorkingDays = 1, totalEmpWage = 0, totalEmpHrs = 0;
         System.out.println("Welcome to Employee Wage Computation Program");
-        for (totalWorkingDays = 1; totalWorkingDays <= MAX_WORKING_DAYS; totalWorkingDays++) {
-            int Empcheck = (int) (Math.random() * 10) % 3;
-            switch (Empcheck) {
-                case 1:
-                    empHr = 8;
-                    break;
-                case 2:
-                    empHr = 4;
-                    break;
-                default:
+        while (totalEmpHrs < MAX_WORKING_HRS && totalWorkingDays < MAX_WORKING_DAYS)
+
+            for (totalWorkingDays = 1; totalWorkingDays <= MAX_WORKING_DAYS; totalWorkingDays++) {
+                int Empcheck = (int) (Math.random() * 10) % 3;
+                switch (Empcheck) {
+                    case FULL_TIME:
+                        System.out.println("Employee is working Full Time  on day " + totalWorkingDays);
+                        empHr = 8;
+                        break;
+                    case PART_TIME:
+                        System.out.println("Employee is working Part Time  on day " + totalWorkingDays);
+                        empHr = 4;
+                        break;
+                    default:
+                        System.out.println("Employee is Absent on day " + totalWorkingDays);
+                        empHr = 0;
+                }
+                totalWorkingDays++;
+                totalEmpHrs = +empHr + totalEmpHrs;
             }
-            empWage = empHr * EMP_WAGE_PER_HR;
-            totalEmpWage = +empWage + totalEmpWage;
-        }
+        totalEmpWage = EMP_WAGE_PER_HR * totalEmpHrs;
+        System.out.println("Employee worked for " + totalWorkingDays + " Days " + totalEmpHrs + " Hours");
         System.out.println("Employee's Monthly wage is " + totalEmpWage);
     }
 }
