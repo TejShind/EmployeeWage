398249c3cded25959cd03838d56605c6997e78eb
TejShind <tej.gholap25@gmail.com>
2022-03-03 17:34:59 +0530

    [Tejaswini]ADDED:Feature for ability to save total Wage for each company

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 7) Calculate wages till condition met(Total working hours=100, Maximum days =20).
 8) Refactor the code by using class method and class variables.
 9) Calculate Employee Wage for multiple companies.
+10) Ability to save total Wage for each company.
--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -1,41 +1,61 @@
 package com.blz.employeewage;
 
 public class EmployeeWage {
+
     public static final int FULL_TIME = 1;
     public static final int PART_TIME = 2;
 
-    public static void main(String[] args) {
-        computeWage("TATA", 50, 20, 22);
-        computeWage("INFOSYS", 35, 20, 25);
+    private static int numOfWorkingDays;
+    private final String company;
+    private final int empRatePerHr;
+    private final int maxHrPerMonth;
+    private int totalWage;
+
+    EmployeeWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHrPerMonth) {
+        this.company = company;
+        this.empRatePerHr = empRatePerHr;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHrPerMonth = maxHrPerMonth;
     }
 
-    private static void computeWage(String companyName, int empRatePerHr, int numOfWorkingDays, int maxHrPerMonth) {
+    public static void main(String[] args) {
+        EmployeeWage company1 = new EmployeeWage("TATA", 35, 20, 22);
+        EmployeeWage company2 = new EmployeeWage("INFOSYS", 25, 20, 20);
+        int c1= company1.computeWage();
+        int c2 = company2.computeWage();
+        System.out.println(c1);
+        System.out.println(c2);
+
+        }
+
+    @Override
+    public String toString() {
+        return "EmployeeWage{" +
+                "company='" + company + '\'' +
+                ", empRatePerHr=" + empRatePerHr +
+                ", numOfWorkingDays=" + numOfWorkingDays +
+                ", maxHrPerMonth=" + maxHrPerMonth +
+                ", totalWage=" + totalWage +
+                '}';
+    }
+
+    private int computeWage() {
         int empHr;
-        int totalWorkingDays = 0, totalEmpWage, totalWorkingHrs = 0;
-        System.out.println("Welcome to Employee Wage Computation Program");
+        int totalWorkingDays = 0, totalWorkingHrs = 0;
         //Monthly Wage Computation
         while (totalWorkingDays <= numOfWorkingDays && totalWorkingHrs <= maxHrPerMonth) {
             int empCheck = (int) (Math.random() * 10) % 3;
             switch (empCheck) {
-                case FULL_TIME -> {
-                    System.out.println("Employee is working Full Time  on day " + totalWorkingDays);
-                    empHr = 8;
-                }
-                case PART_TIME -> {
-                    System.out.println("Employee is working Part Time  on day " + totalWorkingDays);
-                    empHr = 4;
-                }
-                default -> {
-                    System.out.println("Employee is Absent on day " + totalWorkingDays);
-                    empHr = 0;
-                }
+                case FULL_TIME -> empHr = 8;
+                case PART_TIME -> empHr = 4;
+                default -> empHr = 0;
             }
-
             totalWorkingHrs = totalWorkingHrs + empHr;
             System.out.println("Employee worked for " + totalWorkingDays + " Days " + totalWorkingHrs + " Hours");
             totalWorkingDays++;
         }
-        totalEmpWage = empRatePerHr * totalWorkingHrs;
-        System.out.println("Employee's Monthly wage for " + companyName + " is " + totalEmpWage);
+        totalWage = empRatePerHr * totalWorkingHrs;
+        return totalWage;
     }
 }
+
